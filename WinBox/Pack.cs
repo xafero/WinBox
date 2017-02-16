7ab231afb3f8a4c4a2e1c07dc6fd54b3c31501de
xafero <georg.berendt@outlook.com>
2017-02-16 21:53:59 +0100

    Introduced pack file generation

---
+++ WinBox/Pack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace WinBox
+{
+	public class Pack
+	{
+		private static readonly JsonSerializerSettings cfg = new JsonSerializerSettings
+		{
+			Formatting = Formatting.Indented,
+			NullValueHandling = NullValueHandling.Ignore
+		};
+		
+		public List<Builder> builders { get; set; }
+		
+		public Pack()
+		{
+			builders = new List<Builder>();
+		}
+		
+		public override string ToString()
+		{
+			return JsonConvert.SerializeObject(this, cfg);
+		}
+	}
+	
+	public class Builder
+	{
+		public string type { get; set; }
+	}
+}
--- WinBox/Program.cs
+++ WinBox/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Specialized;
 using System.Configuration;
 using System.Diagnostics;
 using System.IO;
+using System.Text;
 using log4net;
 using log4net.Config;
 
@@ -11,7 +12,7 @@ namespace WinBox
 	class Program
 	{
 		private static readonly ILog log = LogManager.GetLogger("winbox");
-		
+
 		public static void Main(string[] args)
 		{
 			BasicConfigurator.Configure();
@@ -22,10 +23,19 @@ namespace WinBox
 			log.InfoFormat("Looking for => {0}", packerExe);
 			if (!File.Exists(packerExe))
 				Shell.DownloadPacker(root, config);
-			Shell.ExecutePacker(packerExe, "shit.json", config);
+			var pack = new Pack
+			{
+				builders = {
+					new Builder {
+						type = "virtualbox-iso"
+					}
+				}
+			};
+			var packFile = Path.Combine(root, "winbox.json");
+			File.WriteAllText(packFile, pack.ToString());
+			Shell.ExecutePacker(packerExe, packFile, config);
 			log.InfoFormat("Have a nice day!");
-			if (Debugger.IsAttached)
-				Debugger.Break();
+			if (Debugger.IsAttached) Debugger.Break();
 		}
 	}
 }
