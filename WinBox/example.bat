453620c80f4be7f4b8194c0e6335ff1d65149a79
xafero <georg.berendt@outlook.com>
2017-02-18 23:34:13 +0100

    Added support for Win 7 32 bit

--- WinBox/example.bat
+++ WinBox/example.bat
@@ -1,2 +1,3 @@
 WinBox -Dguest=Windows2012_64 -Dkey=D2N9P-3P6X9-2R39C-7RTCD-MDVJX
 WinBox -Dguest=Windows7_64    -Dkey=HYF8J-CVRMY-CM74G-RPHKF-PW487
+WinBox -Dguest=Windows7       -Dkey=HYF8J-CVRMY-CM74G-RPHKF-PW487
